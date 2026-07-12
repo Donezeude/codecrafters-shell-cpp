7656e378101b0483a2fa58e16929310d417b6d02
Donezeude <ezeude.donald@gmail.com>
2026-07-12 15:39:48 -0400

    codecrafters submit [skip ci]

--- src/minors/exec_program.cpp
+++ src/minors/exec_program.cpp
@@ -64,7 +64,7 @@ std::vector<std::string> single_tokenize(const std::string& input)
 	return elements;
 }
 
-std::string backslash(std::string& phrase)
+std::string exec_backslash(std::string& phrase)
 {
 	phrase.erase(std::remove(phrase.begin(), phrase.end(), '\\'), phrase.end());
 	return phrase;
@@ -89,7 +89,7 @@ pid_t exec_program(const std::string& command, std::string& input, const std::st
 
 		if(access(command_path.c_str(), X_OK)==0)
 		{
-			std::stringstream input_ss(backslash(input));
+			std::stringstream input_ss(exec_backslash(input));
 			
 			std::vector<char*> args;
 			std::vector<std::string> elements;
